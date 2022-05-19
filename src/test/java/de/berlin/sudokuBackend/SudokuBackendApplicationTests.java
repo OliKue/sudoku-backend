865aa35e79a3cf8fec3be60159dfff6f7eca01a9
OliKue# <oliver-kuetemeier123@web.de>
2022-05-19 10:12:31 +0200

    spring init

---
+++ src/test/java/de/berlin/sudokuBackend/SudokuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.berlin.sudokuBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
