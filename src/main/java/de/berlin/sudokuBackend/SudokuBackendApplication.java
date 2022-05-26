445ef62f260154848c19ea767cd8792cd2437a77
OliKue# <oliver-kuetemeier123@web.de>
2022-05-26 14:41:58 +0200

    add corsConfig

--- src/main/java/de/berlin/sudokuBackend/SudokuBackendApplication.java
+++ src/main/java/de/berlin/sudokuBackend/SudokuBackendApplication.java
@@ -2,12 +2,24 @@ package de.berlin.sudokuBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SudokuBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SudokuBackendApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/sudoku/*").allowedOrigins("https://my5ud0ku-app.herokuapp.com");
+			}
+		};
 	}
 
 }
